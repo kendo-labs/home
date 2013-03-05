976347046ea2209374d0894057544c801e46a795
Brandon Satrom <bsatrom+gh@gmail.com>
2013-03-05 17:02:25 -0600

    added started logic to pull repo info from GitHub

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,23 +2,8 @@
 	var projectsList, projects, ds, template;
 
 	projectList = $('#projectsList');
-	projects = [ {
-			projectName: "angular-kendo",
-			projectURL: "",
-			lastRelease: "",
-			currentVersion: "",
-			lastCommitTime: "",
-			lastCommitUser: "bsatrom"
-		},
-		{
-			projectName: "breeze-kendo",
-			projectURL: "",
-			lastRelease: "",
-			currentVersion: "",
-			lastCommitTime: "",
-			lastCommitUser: "derickbailey"
-		}
-	];
+	projects = [];
+
 	ds = new kendo.data.DataSource({ data: projects });
 	template = kendo.template($('#projectItemTemplate').html());
 
@@ -27,5 +12,24 @@
 		dataSource: ds
 	});
 	ds.read();
+
+	$.get("https://api.github.com/orgs/kendo-labs/repos").done(function(repos) {
+		$.each(repos, function(repo) {
+			var tagsURL, commitsURL;
+			var item = {
+				projectName: repo.name,
+				projectURL: repo.html_url,
+				lastRelease: "",
+				currentVersion: "",
+				lastCommitTime: repo.pushed_at,
+				lastCommitUser: ""
+			};
+
+			tagsURL = repo.tags_url;
+			commitsURL = repo.commits_url;
+
+			ds.add(item);
+		});
+	});
 
 }($, kendo));
