98aee17884aad2aa2b770cf08fde77ade921ec6f
Brandon Satrom <bsatrom+gh@gmail.com>
2013-03-06 11:40:18 -0600

    added oauth to api calls

--- javascripts/app.js
+++ javascripts/app.js
@@ -14,9 +14,13 @@
 	ds.read();
 
 	$.ajax({
-		url: "https://api.github.com/orgs/kendo-labs/repos", 
+		url: "https://api.github.com/orgs/kendo-labs/repos" + appendAuth(), 
 		dataType: "jsonp"
 	}).done(function(repos) {
+			if (repos.message) {
+				return;
+			}
+
 			$.each(repos.data, function(index, repo) {
 				var tagsURL, commitsURL;
 				var item = {
@@ -33,7 +37,7 @@
 				tagsURL = trimGitHubURL(repo.tags_url);
 
 				$.ajax({
-					url: commitsURL,
+					url: commitsURL + appendAuth(),
 					dataType: "jsonp"
 				}).done(function(commits) {
 					if (commits.data.length > 0) {
@@ -41,7 +45,7 @@
 					}
 
 					$.ajax({
-						url: tagsURL,
+						url: tagsURL + appendAuth(),
 						dataType: "jsonp"
 					}).done(function(tags) {
 						if (tags.data.length > 0) {
@@ -49,7 +53,7 @@
 							item.currentVersionURL = tags.data[0].zipball_url;
 
 							$.ajax({
-								url: tags.data[0].commit.url,
+								url: tags.data[0].commit.url + appendAuth(),
 								dataType: "jsonp"
 							}).done(function(commitData) {
 								if (commitData.data.commit) {
@@ -74,6 +78,10 @@
 
 	function trimGitHubURL(url) {
 		return url.slice(0, url.indexOf("{"));
+	}
+
+	function appendAuth() {
+		return "?client_id=99037c7dfd5c47cdd24d&client_secret=d2a6d2c157ae1ef17e6c6876f4edb3b1da7f0d15";
 	}
 
 }($, kendo, moment));
